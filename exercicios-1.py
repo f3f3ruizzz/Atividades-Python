a99a26e9691358850dc24cb3cd2dfe9dbd52d22f
Fernanda Ruiz <nanda.rm1810@gmail.com>
2025-03-11 14:37:05 -0300

    Add files via upload

---
+++ exercicios-1.py
@@ -0,0 +1,69 @@
+
+import os
+
+#Limpando a tela
+os.system("cls")
+
+#1 Passo - Entrada de dados 
+print('''ℂ𝕠𝕟𝕧𝕖𝕣𝕤𝕒̃𝕠 𝕕𝕖 ℂ𝕖𝕝𝕤𝕚𝕦𝕤 𝕡𝕒𝕣𝕒 𝔽𝕒𝕙𝕣𝕖𝕟𝕙𝕖𝕚𝕥.''')
+print("===================================== \n")
+
+C = input("Digite o numero que deseja converter: ")
+
+#2 Passo - Realizar procedimento 
+f = (9*C+160) / 5, "sendo que: F = Fahrenheit C - Celsius: "
+
+#3 Passo - Saída de dados 
+("Resultado da conversão:", f )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
